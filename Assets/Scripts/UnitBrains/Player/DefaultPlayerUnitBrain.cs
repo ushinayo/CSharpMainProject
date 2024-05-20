a79cca23f39ce4cb74cdbcb24b0e0613637405fc
ushinayo <adianova.kot@mail.ru>
2024-05-20 23:56:45 +0300

    dz_10 usha

--- Assets/Scripts/UnitBrains/Player/DefaultPlayerUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/DefaultPlayerUnitBrain.cs
@@ -1,12 +1,55 @@
 ﻿using System.Collections.Generic;
 using Model;
 using Model.Runtime.Projectiles;
+using UnitBrains.Pathfinding;
 using UnityEngine;
 
 namespace UnitBrains.Player
 {
     public class DefaultPlayerUnitBrain : BaseUnitBrain
     {
+        private BaseUnitPath _activePath;
+        private int _coordinatorRange = 2;
+
+        public override Vector2Int GetNextStep()
+        {
+            var recommendedTarget = PlayerUnitCoordinator.GetInstance().RecommendedTarget;
+            var recommendedPosition = PlayerUnitCoordinator.GetInstance().RecommendedPosition;
+
+            _activePath = new AlgoritmAUnitPath(runtimeModel, unit.Pos, recommendedPosition);
+
+            if (IsTargetInCoordinationRange(recommendedTarget))
+            {
+                if (IsTargetInRange(recommendedTarget))
+                    return unit.Pos;
+
+                _activePath = new AlgoritmAUnitPath(runtimeModel, unit.Pos, recommendedTarget);
+            }
+
+            return _activePath.GetNextStepFrom(unit.Pos);
+        }
+
+        private bool IsTargetInCoordinationRange(Vector2Int targetPosition)
+        {
+            var detectionRange = _coordinatorRange * unit.Config.AttackRange * unit.Config.AttackRange;
+            var v = targetPosition - unit.Pos;
+
+            return v.sqrMagnitude <= detectionRange;
+        }
+
+        protected override List<Vector2Int> SelectTargets()
+        {
+            var result = new List<Vector2Int>();
+
+            var targets = GetReachableTargets();
+            var recommendedTarget = PlayerUnitCoordinator.GetInstance().RecommendedTarget;
+
+            if (targets.Contains(recommendedTarget))
+                result.Add(recommendedTarget);
+
+            return result;
+
+        }
         protected float DistanceToOwnBase(Vector2Int fromPos) =>
             Vector2Int.Distance(fromPos, runtimeModel.RoMap.Bases[RuntimeModel.PlayerId]);
 
---
+++ Assets/Scripts/UnitBrains/Player/PlayerUnitCoordinator.cs
@@ -0,0 +1,140 @@
+﻿using GluonGui.WorkspaceWindow.Views.WorkspaceExplorer.Explorer;
+using Model;
+using Model.Runtime.ReadOnly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Utilities;
+
+
+namespace UnitBrains.Player
+{
+    public class PlayerUnitCoordinator : IDisposable
+    {
+        public Vector2Int RecommendedTarget { get; private set; }
+        public Vector2Int RecommendedPosition { get; private set; }
+
+        private static PlayerUnitCoordinator _instance;
+        private IReadOnlyRuntimeModel _runtimeModel;
+        private TimeUtil _timeUtil;
+        private bool _enemyOnPlayerSide;
+        private float _attackRange;
+
+        private PlayerUnitCoordinator()
+        {
+            _runtimeModel = ServiceLocator.Get<IReadOnlyRuntimeModel>();
+            _timeUtil = ServiceLocator.Get<TimeUtil>();
+
+            _attackRange = _runtimeModel.RoPlayerUnits.First().Config.AttackRange;
+            _timeUtil.AddFixedUpdateAction(UpdatePlayerUnitCoordinator);
+        }
+
+        public static PlayerUnitCoordinator GetInstance()
+        {
+            if (_instance == null)
+                _instance = new PlayerUnitCoordinator();
+
+            return _instance;
+        }
+
+        private void UpdatePlayerUnitCoordinator(float deltaTime)
+        {
+            var botUnits = _runtimeModel.RoBotUnits.ToList();
+
+            if (botUnits.Count == 0)
+            {
+                var botBasePosition = _runtimeModel.RoMap.Bases[RuntimeModel.BotPlayerId];
+                RecommendedTarget = botBasePosition;
+                RecommendedPosition = botBasePosition;
+            }
+            else
+            {
+                CheckPlayerSideForEnemies();
+                GetRecommendedTarget(botUnits);
+                GetRecommendedPosition(botUnits);
+            }
+
+        }
+
+        public void GetRecommendedTarget(List<IReadOnlyUnit> botUnits)
+        {
+            if (_enemyOnPlayerSide)
+                SortByDistanceToPlayerBase(botUnits);
+
+            else SortByHealth(botUnits);
+
+            RecommendedTarget = botUnits.First().Pos;
+        }
+
+        public void GetRecommendedPosition(List<IReadOnlyUnit> botUnits)
+        {
+            if (_enemyOnPlayerSide)
+                RecommendedPosition = _runtimeModel.RoMap.Bases[RuntimeModel.PlayerId] + Vector2Int.up;
+
+            else
+            {
+                SortByDistanceToPlayerBase(botUnits);
+
+                var x = botUnits.First().Pos.x;
+                var y = botUnits.First().Pos.y - (int)Math.Round(_attackRange);
+
+                RecommendedPosition = new Vector2Int(x, y);
+            }
+        }
+
+        private void CheckPlayerSideForEnemies()
+        {
+            _enemyOnPlayerSide = false;
+
+            int playerBaseY = _runtimeModel.RoMap.Bases[RuntimeModel.PlayerId].y;
+            int botBaseY = _runtimeModel.RoMap.Bases[RuntimeModel.BotPlayerId].y;
+            int distanceToCenter = (botBaseY - playerBaseY) / 2;
+
+
+            foreach (var unit in _runtimeModel.RoBotUnits)
+            {
+                if ((unit.Pos.y - playerBaseY) <= distanceToCenter)
+                {
+                    _enemyOnPlayerSide = true;
+                    return;
+                }
+            }
+        }
+
+        private void SortByHealth(List<IReadOnlyUnit> units)
+        {
+            units.Sort(CompareByHealth);
+        }
+
+        private void SortByDistanceToPlayerBase(List<IReadOnlyUnit> units)
+        {
+            units.Sort(CompareByDistanceToPlayerBase);
+        }
+
+        private int CompareByHealth(IReadOnlyUnit unitA, IReadOnlyUnit unitB)
+        {
+            var healthA = unitA.Health;
+            var healthB = unitB.Health;
+
+            return healthA.CompareTo(healthB);
+        }
+        private int CompareByDistanceToPlayerBase(IReadOnlyUnit unitA, IReadOnlyUnit unitB)
+        {
+            var playerBaseId = _runtimeModel.RoMap.Bases[RuntimeModel.PlayerId];
+            var distanceA = Vector2Int.Distance(unitA.Pos, playerBaseId);
+            var distanceB = Vector2Int.Distance(unitB.Pos, playerBaseId);
+
+            return distanceA.CompareTo(distanceB);
+        }
+
+        public void Dispose()
+        {
+            _timeUtil.RemoveFixedUpdateAction(UpdatePlayerUnitCoordinator);
+        }
+    }
+}
